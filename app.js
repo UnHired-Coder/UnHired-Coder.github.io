26110fff8f2fa9dd126b065629a34f6349cdc453
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2021-06-06 17:44:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function alertMessage(text) {
+    alert("Tum kya ?")
+//    document.getElementById('adobe-dc-view').innerHTML = Date();
+      document.addEventListener("adobe_dc_view_sdk.ready", function(){
+          var adobeDCView = new AdobeDC.View({clientId: "12bf96896da7438f8865e5616ad746c2", divId: "adobe-dc-view"});
+                 adobeDCView.previewFile({
+                   content:{ location:
+                     { url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea%20Brochure.pdf"}},
+                   metaData:{fileName: "Bodea Brochure.pdf"}
+                 },
+                 {
+                   embedMode: "SIZED_CONTAINER"
+          });
+      });
+  }
+
+window.logger = (flutter_value) => {
+   console.log({ js_context: this, flutter_value });
+}
