669bead0dc2b6f1c083d12008a1d3f98e6319af6
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2021-06-06 18:02:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function alertMessage(text) {
-    alert("Tum kya ?")
+//    alert("Tum kya ?")
 //    document.getElementById('adobe-dc-view').innerHTML = Date();
       document.addEventListener("adobe_dc_view_sdk.ready", function(){
+        alert("Step 1")
           var adobeDCView = new AdobeDC.View({clientId: "12bf96896da7438f8865e5616ad746c2", divId: "adobe-dc-view"});
                  adobeDCView.previewFile({
                    content:{ location:
